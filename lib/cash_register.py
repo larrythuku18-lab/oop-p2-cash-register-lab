888004f684146ea1fd07c2eb9b7042958a5fcf72
Larry Thuku <larrythuku18@gmail.com>
2026-06-10 14:32:50 +0300

    Add discount property with validation

--- lib/cash_register.py
+++ lib/cash_register.py
@@ -7,3 +7,14 @@ class CashRegister:
         self.total = 0
         self.items = []
         self.previous_transactions = []
+
+    @property
+    def discount(self):
+        return self._discount
+
+    @discount.setter
+    def discount(self, discount):
+        if isinstance(discount, int) and 0 <= discount <= 100:
+            self._discount = discount
+        else:
+            print("Not valid discount")
