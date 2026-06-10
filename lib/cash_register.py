91818fd03c63c5f3e188c9b057c507775ca89988
Larry Thuku <larrythuku18@gmail.com>
2026-06-10 14:32:38 +0300

    Add CashRegister __init__ with discount, total, items, previous_transactions

--- lib/cash_register.py
+++ lib/cash_register.py
@@ -1,4 +1,9 @@
 #!/usr/bin/env python3
 
 class CashRegister:
-  pass
+
+    def __init__(self, discount=0):
+        self.discount = discount
+        self.total = 0
+        self.items = []
+        self.previous_transactions = []
